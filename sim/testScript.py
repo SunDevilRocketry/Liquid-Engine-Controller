351638891d8a319b772fc325c0fba88871f54ba7
Colton Acosta <65829250+cacost12@users.noreply.github.com>
2021-07-10 20:02:38 -0700

    Delete testScript.py

--- sim/testScript.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# Testscript for ics.py module
-
-Test`
-import ics
-
-buckConv = ics.buck('LM22672MRE-5.0/NOPB')
-buckConv.calcL('LM22672MRE-5.0/NOPB')
-print((buckConv.Lrecomended*1e6), ' uH')
